1f8c3cfb83339e9712e9fdc726261d6b17835548
atippaz <nasakun13201@gmail.com>
2024-07-06 09:42:12 +0700

    add return value

--- KasetMore/Controllers/AuthController.cs
+++ KasetMore/Controllers/AuthController.cs
@@ -54,7 +54,7 @@ namespace KasetMore.Controllers
                     return BadRequest();
                 }
                 await _userRepository.UpdateProfilePicture(file, email);
-                return Ok();
+                return Ok("success");
             }
             catch (Exception e)
             {
@@ -71,7 +71,7 @@ namespace KasetMore.Controllers
                     return BadRequest();
                 }
                 await _userRepository.UpdateVerifyFlag(email, flag);
-                return Ok();
+                return Ok("success");
             }
             catch (Exception e)
             {
@@ -84,7 +84,7 @@ namespace KasetMore.Controllers
             try
             {
                 await _userRepository.UpdateProfile(userDto);
-                return Ok();
+                return Ok("success");
             }
             catch (Exception e)
             {
--- KasetMore/Controllers/ProductController.cs
+++ KasetMore/Controllers/ProductController.cs
@@ -69,7 +69,7 @@ namespace KasetMore.Controllers
             try
             {
                 await _productRepository.UpdateProduct(product);
-                return Ok();
+                return Ok("success");
             }
             catch (Exception e)
             {
@@ -82,7 +82,7 @@ namespace KasetMore.Controllers
             try
             {
                 await _productRepository.AddProduct(product, images);
-                return Ok();
+                return Ok("success");
             }
             catch (Exception e)
             {
@@ -95,7 +95,7 @@ namespace KasetMore.Controllers
             try
             {
                 await _productRepository.DeleteProduct(id);
-                return Ok();
+                return Ok("success");
             }
             catch (Exception e)
             {
@@ -108,7 +108,7 @@ namespace KasetMore.Controllers
             try
             {
                 await _productRepository.DeleteProductImages(ids);
-                return Ok();
+                return Ok("success");
             }
             catch (Exception e)
             {
